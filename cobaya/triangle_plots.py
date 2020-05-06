a1e7bf867af5a44852c1a9634867a425e651307e
Aleksandr Petrosyan <a-p-petrosyan@Yandex.ru>
2020-05-06 14:05:34 +0400

    Final touchups.

--- cobaya/triangle_plots.py
+++ cobaya/triangle_plots.py
@@ -10,17 +10,17 @@ import pandas
 matplotlib.use('Qt5Agg')
 # g = plots.get_subplot_plotter(chain_dir=r'/home/app/Git/LCDM-NS/cobaya/cobaya_output')
 
-chains = loadMCSamples(r'/home/app/Git/LCDM-NS/cobaya/cobaya_output/run')
+# chains = loadMCSamples(r'/home/app/Git/LCDM-NS/cobaya/cobaya_output/run)
+chains = loadMCSamples(r'/Users/app/Git/LCDM-NS/cobaya/cobaya_output/run')
 # roots = ['run']
 params = ['logA', 'n_s', 'theta_s_1e2', 'omega_b', 'omega_cdm', 'tau_reio']
 # g.triangle_plot(roots, params, filled=True, shaded=True)
 # plt.show()
 
 # The fact that this doesn't work, is living proof that Anthony Lewis should stick to science.
-# This is very poorly thought out, and I'm surprised so many people are using any of his programs. 
+# This is very poorly thought out, and I'm surprised so many people are using any of his programs.
 # print(chains.getMeans(['logA']))
 
 chains.getParamNames().saveAsText('paramnames')
-np.savetxt('means', chains.getMeans(range(27)))
 
-np.savetxt('covs', chains.getCov(pars=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]))
+print(np.linalg.inv(chains.getCov(27)))
